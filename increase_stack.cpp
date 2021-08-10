5ccf8b9fd21458c707d32d4db270ccfb5702f52d
ramit19086 <58386250+ramit19086@users.noreply.github.com>
2021-08-10 15:21:19 +0530

    Update increase_stack.cpp

--- increase_stack.cpp
+++ increase_stack.cpp
@@ -10,3 +10,8 @@ void func()
 }
 #define int long long    // its important you declare this line after the function as during 
                         // compiling it will change the int to long long which will give compilation errors
+
+//In competitive programming sometimes it's the case that you declare some arrays or makes recursive functions 
+//that go for like 2e5 iterations and then your system then you get a run time error as SIGSEGV or in this specific case
+//stack overflow. The reason for this is that by default stack size is limited to some 1MB but if you want to run the big test case
+//then in that case you would have to increase the stack size. The function above increases the stack size to the memory you specify.
