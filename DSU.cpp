0ae6323af64aaa4c72108fe21fe5430773ec3a55
ramit19086 <58386250+ramit19086@users.noreply.github.com>
2021-08-10 15:29:13 +0530

    Update DSU.cpp

--- DSU.cpp
+++ DSU.cpp
@@ -1,36 +1,34 @@
+include <bits/stdc++.h>
+using namespace std;
 class DSU
 {
 private:
-    int *rank, *parent, n;
-
+    int n;
+    vector<int> rank, parent;
 public:
     DSU(int n = 1) : n(n)
     {
-        rank = new int[n + 1], parent = new int[n + 1];
+        rank = vector<int>(n + 1), parent = vector<int>(n + 1);
         for (int i = 1; i <= n; i++)
             parent[i] = i;
     }
-    int find_set(int v)
+    int Find(int v)
     {
         if (v == parent[v])
             return v;
-        return parent[v] = find_set(parent[v]);
+        return parent[v] = Find(parent[v]);
     }
 
-    void union_sets(int a, int b)
+    bool Union(int a, int b)
     {
-        a = find_set(a), b = find_set(b);
-        if (a != b)
-        {
-            if (rank[a] < rank[b])
-                swap(a, b);
-            parent[b] = a;
-            if (rank[a] == rank[b])
-                rank[a]++;
-        }
-    }
-    ~DSU()
-    {
-        delete[] rank, delete[] parent;
+        a = Find(a), b = Find(b);
+        if (a == b)
+            return 0;
+        if (rank[a] < rank[b])
+            swap(a, b);
+        parent[b] = a;
+        if (rank[a] == rank[b])
+            rank[a]++;
+        return 1;
     }
 };
