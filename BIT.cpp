1da3ee1b7deffd30b86d1714d3e1228af50d65d8
ramit19086 <58386250+ramit19086@users.noreply.github.com>
2021-09-13 13:42:46 +0530

    Update BIT.cpp

--- BIT.cpp
+++ BIT.cpp
@@ -1,45 +1,35 @@
-#include<bits/stdc++.h>
-using namespace std;
 template<typename T>
 class BIT
 {
 public:
     vector<T> tree;
-    int maxval;
+    int N;
     BIT(int n, int initial_val) {
-        tree = vector<T>(n+1, initial_val);
-        maxval = n;
+        tree = vector<T>(n + 1, initial_val);
+        N = n;
     }
 
-    T todo(T a, T b) {     //can be modified accordingly
+    T todo(T a, T b) {
         return a + b;
     }
 
-    void build(vector<T> a) {                 // 1 based indexing 
-        for(int i = 1; i <= maxval; i++) {
+    void build(vector<T> a) {
+        for (int i = 1; i <= N; i++) {
             int idx = i;
-            while(idx <= maxval) {
-                tree[idx] = todo(tree[idx],a[i]);
-                idx += (idx & -idx);
-            }
+            for (;idx <= N; idx += idx & (-idx))
+                tree[idx] = todo(tree[idx], a[i]);
         }
     }
 
-    void update(int idx , T val) {
-        assert(idx > 0);
-        while(idx <= maxval) {
-            tree[idx] = todo(tree[idx],val);
-            idx += (idx & -idx);
-        }
+    void update(int idx, T val) {
+        for (; idx <= N; idx += idx & (-idx))
+            tree[idx] = todo(tree[idx], val);
     }
 
     T get(int idx) {
-        assert(idx > 0);
         T res = 0;
-        while(idx > 0) {
+        for (; idx > 0; idx -= idx & (-idx))
             res = todo(res, tree[idx]);
-            idx -= (idx & -idx); 
-        }  
         return res;
-    } 
+    }
 };
