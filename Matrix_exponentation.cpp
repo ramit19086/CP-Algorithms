fb6ebe3c7bec9b9c8bc30e8a6ddc73d112a9d40a
ramit19086 <58386250+ramit19086@users.noreply.github.com>
2022-01-09 14:45:54 +0530

    Update Matrix_exponentation.cpp

--- Matrix_exponentation.cpp
+++ Matrix_exponentation.cpp
@@ -5,7 +5,6 @@ vvi multiply_matrix(vvi a, vvi b)
 {
     int n = a.size();
     vvi mul(n, vi(n));
-    // O(n^3)
     for (int i = 0; i < n; i++)
         for (int j = 0; j < n; j++)
             for (int k = 0; k < n; k++)
